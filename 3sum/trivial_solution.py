d628a85c6fe6613b07a99dd88fc0e9aa49ba62b1
Anna Chaykovsky <annachaykovsky@gmail.com>
2024-07-10 09:31:47 +0300

    created the trivial_solution.py for 3sum

---
+++ 3sum/trivial_solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+# Time Complexity: O(n^3)
+# Space Complexity: O(n^3)
+def threeSum(nums: List[int]) -> List[List[int]]:
+    result = set()
+    num_length = len(nums)
+    for i in range(num_length):
+        for j in range(num_length):
+            for k in range(num_length):
+                if i < j < k and nums[i] + nums[j] + nums[k] == 0:
+                    triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
+                    if triplet not in result:
+                        result.add(triplet)
+
+    return [list(triplet) for triplet in result]
+
+
+if __name__ == '__main__':
+    nums = [-1, 0, 1, 2, -1, -4]
+    result = threeSum(nums)
+    print(result)
