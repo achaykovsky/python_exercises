51f8553edc71122afd16028e33126c55428db8e2
Anna Chaykovsky <annachaykovsky@gmail.com>
2024-08-04 19:02:47 +0300

    updated the optimal_solution.py for merge-sorted-arrays

--- merge-sorted-arrays/optimal_solution.py
+++ merge-sorted-arrays/optimal_solution.py
@@ -1,41 +1,30 @@
-# This is a generic file for the trivial solution
-import heapq
 from typing import List
 
 
-# Time Complexity: O(m+n) Space: O(m+n)
-def merge_sorted_arrays(array1: List[int], array2: List[int]) -> List[int]:
-    result = []
-    array1_length, array2_length = len(array1), len(array2)
-    p1, p2 = 0, 0
+# Time Complexity: O(num1_length + num2_length)
+# Space: O(1)
+def merge_sorted_arrays(nums1: List[int], num1_length: int, nums2: List[int], num2_length: int) -> None:
+    end = num1_length + num2_length - 1  # last element index
 
-    # This block will work fully on same size arrays
-    while p1 < array1_length and p2 < array2_length:
-        if array1[p1] == array2[p2]:
-            result.append(array1[p1])
-            result.append(array2[p2])
-        elif array1[p1] > array2[p2]:  # the equation will decide the order of the insertion of the numbers
-            result.append(array2[p2])
-            result.append(array1[p1])
+    while num2_length > 0 and num1_length > 0:
+        if nums1[num1_length - 1] > nums2[num2_length - 1]:
+            nums1[end] = nums1[num1_length - 1]
+            num1_length -= 1
         else:
-            result.append(array1[p1])
-            result.append(array2[p2])
-        p2 += 1
-        p1 += 1
+            nums1[end] = nums2[num2_length - 1]
+            num2_length -= 1
+        end -= 1
 
-    while p1 < array1_length:  # adding all the rest of array1, if we didn't reach the ending before
-        result.append(array1[p1])
-        p1 += 1
+    while num2_length > 0:
+        nums1[end] = nums2[num2_length - 1]
+        end -= 1
+        num2_length -= 1
 
-    while p2 < array2_length:  # adding all the rest of array2, if we didn't reach the ending before
-        result.append(array2[p2])
-        p2 += 1
-
-    return result
+    return nums1
 
 
 if __name__ == '__main__':
-    array1 = [1, 2, 7, 8, 9]
-    array2 = [0, 1, 3]
-    result = merge_sorted_arrays(array1, array2)
+    array1 = [1, 2, 3, 0, 0, 0]  # we have enough space to add array2
+    array2 = [2, 2, 2]
+    result = merge_sorted_arrays(array1, 3, array2, 3)
     print(result)
