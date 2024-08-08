43aa6fbc76d0a666896e856318bc371ca15b90bb
Anna Chaykovsky <annachaykovsky@gmail.com>
2024-08-08 09:14:25 +0300

    updated the trivial_version.py for encrypt-message (pramp)

--- encrypt-message/trivial_solution.py
+++ encrypt-message/trivial_solution.py
@@ -2,23 +2,25 @@ from math import ceil
 
 
 # Time complexity: O(n), Space Complexity: O(n)
+
+# encryption function: enc[n] = dec[n] + secondStep[n-1] - 26m
 def encrypt(word):
     result = []
-    UPPER_BOUND = ord("z")  # 122
+    UPPER_BOUND = ord("z")
 
     for c in word:
         result.append(ord(c))
 
     for i, c in enumerate(result, start=1):
         if i == 0:
-            result[i] += 1
+            result[i] = 1  # first letter
         if i < len(word):
-            result[i] = result[i] + result[i - 1]
+            result[i] = result[i] + result[i - 1]  # rest of the letters
 
     for i, c in enumerate(result):
-        differnece = c - UPPER_BOUND
-        number_of_multiplication = ceil(differnece / 26)
-        new_diff = number_of_multiplication * 26 - differnece
+        difference = c - UPPER_BOUND
+        number_of_multiplication = ceil(difference / 26)
+        new_diff = number_of_multiplication * 26 - difference
         result[i] = chr(UPPER_BOUND - new_diff + 1)
 
     return "".join(result)
