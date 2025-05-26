974eb366052590aaf2b56b6fa6270321c05359c2
Anna Chaykovsky <annachaykovsky@gmail.com>
2025-05-26 12:02:05 +0300

    add comments to trivial_solution.py for maximum 69 number problem

--- maximum-69-number/trivial_solution.py
+++ maximum-69-number/trivial_solution.py
@@ -1,3 +1,7 @@
+# Problem: https://leetcode.com/problems/maximum-69-number/
+
+# Time Complexity: O(d^2) - since we are creating a new string in each iteration
+# Space Complexity: O(d)
 def maximum69Number(num: int) -> int:
     num_str = str(num)
     new_num = ""
@@ -5,7 +9,7 @@ def maximum69Number(num: int) -> int:
     for digit in num_str:
         # we will enter here only once when updated is False and change it to True
         if digit == '6' and not updated:
-            new_num += '9'
+            new_num += '9'  # makes a new string, so complexity is O(d)
             updated = True
         else:
             new_num += digit
