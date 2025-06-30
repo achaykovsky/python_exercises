8294c5c5e690c3822a9312fb105e8a9cb4fe6f04
Anna Chaykovsky <annachaykovsky@gmail.com>
2025-06-30 13:28:26 +0300

    add linked_lists_utils.py enhancements for creating linked lists with cycles and appending lists

--- utils/linked_lists_utils.py
+++ utils/linked_lists_utils.py
@@ -7,7 +7,7 @@ class ListNode:
         self.next = next
 
 
-def create_linked_list(arr: List[int]) -> ListNode:
+def create_linked_list_from_array(arr: List[int]) -> ListNode:
     if not arr:  # If the list is empty, return None
         return None
 
@@ -22,7 +22,33 @@ def create_linked_list(arr: List[int]) -> ListNode:
 
 
 def print_linked_list(head: ListNode) -> None:
+    output = ["head"]
     current = head
     while current:
-        print(current.val)
+        output.append(str(current.val))
         current = current.next
+    output.append("null")
+    print(" -> ".join(output))
+
+
+def create_linked_list_with_cycle(arr: [int], pos: int) -> ListNode:
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    current = head
+    cycle_node = None
+    for i in range(1, len(arr)):
+        current.next = ListNode(arr[i])
+        current = current.next
+        if i == pos:
+            cycle_node = current
+    if pos != -1:
+        current.next = cycle_node
+    return head
+
+
+def append_linked_lists(head1: ListNode, head2: ListNode):
+    current = head1
+    while current.next:
+        current = current.next
+    current.next = head2
