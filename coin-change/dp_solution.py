09d6a4f33350df501f104c1bb1f9b849610abd91
Anna Chaykovsky <annachaykovsky@gmail.com>
2025-07-21 15:55:01 +0300

    refactor dp_solution.py to improve coinChange function and enhance readability

--- coin-change/dp_solution.py
+++ coin-change/dp_solution.py
@@ -5,18 +5,23 @@ from typing import List
 # Space Complexity: O(m)
 
 # DP - bottom up
-def coinChange(coins: List[int], amount: int) -> int:
-    dp = [float('inf')] * (amount + 1)
+def coinChange(coins: List[int], amount: int) -> float:
+    dp = [float('inf')] * (amount + 1)  # dp[i] = min number of coins to make amount i
     dp[0] = 0
 
     for coin in coins:
-        for i in range(coin, amount + 1):
-            dp[i] = min(dp[i], 1 + dp[i - coin])
+        for curr_amount in range(coin, amount + 1):
+            dp[curr_amount] = min(dp[curr_amount], 1 + dp[
+                curr_amount - coin])  # 1 + dp[curr_amount - coin] means using one more coin of type coin
 
-    return dp[amount] if dp[amount] != float('inf') else -1
+    if dp[amount] == float('inf'):  # amount cannot be made up by any combination of coins
+        return -1
+
+    return dp[amount]  # return the minimum number of coins needed to make up the amount
 
 
 if __name__ == '__main__':
     coins = [1, 2, 5]
     amount = 11
     print(coinChange(coins, amount))
+    # Output: 3 (11 = 5 + 5 + 1)
