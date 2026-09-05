d249706acdb389457e6a64cc2ab74f92ff6dc48f
Anna Chaykovsky <annachaykovsky@gmail.com>
2026-09-05 21:15:49 +0300

    refactor trivial_solution.py to improve linked list creation and enhance code clarity

--- add-two-numbers/trivial_solution.py
+++ add-two-numbers/trivial_solution.py
@@ -1,14 +1,18 @@
 from typing import Optional
 
-from utils.linked_lists_utils import ListNode, create_linked_list, print_linked_list
+from utils.linked_lists_utils import ListNode, create_linked_list_from_array, print_linked_list
 
 
+# Time Complexity: O(max(m,n))
+# Space Complexity: O(max(m,n))
 def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     result = ListNode()
     cur = result
     carry = 0
 
     while l1 or l2 or carry:
+        # Validate that l1/l2 is not None because we might have different lengths
+        # If one of them is None, we consider its value as 0
         val1 = l1.val if l1 else 0
         val2 = l2.val if l2 else 0
 
@@ -17,6 +21,7 @@ def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[Li
         curr_digit = curr_digit % 10
         cur.next = ListNode(curr_digit)
 
+        # Move to the next nodes
         cur = cur.next
         if l1:
             l1 = l1.next
@@ -30,8 +35,8 @@ if __name__ == '__main__':
     l1 = [7]
     l2 = [8]
 
-    head1 = create_linked_list(l1)
-    head2 = create_linked_list(l2)
+    head1 = create_linked_list_from_array(l1)
+    head2 = create_linked_list_from_array(l2)
 
     result = addTwoNumbers(head1, head2)
     print_linked_list(result)
